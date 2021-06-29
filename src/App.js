acfea058da4828c20c2c4254189db65d7997e96e
Shahfiq Shah <48479663+Gr3gg0r@users.noreply.github.com>
2021-06-29 16:30:25 +0800

    Update App.js
    Implement function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import react from "react";
 import "./App.css";
 
 const items = [
@@ -16,56 +15,60 @@ const items = [
   },
 ];
 
+function removeItem(index) {
+  items.splice(index,1);
+}
+
 const listItem = items.map((item, index) => {
   return (
     <li key={index}>
       <div class="view">
         <input class="toggle" type="checkbox" />
         <label>{item.title}</label>
-        <button class="destroy" onClick={alert(item.name)}></button>
+        <button class="destroy" onClick={()  =>  removeItem(index)}></button>
       </div>
     </li>
   );
 });
 
-class App extends react.Component {
-  render() {
+function App() {
+  return (
     <div className="todoapp">
-    <header className="header">
-      <h1>todos</h1>
-      <input
-        class="new-todo"
-        placeholder="What needs to be done?"
-        autofocus
-      />
-    </header>
-    <section class="main">
-      <input id="toggle-all" class="toggle-all" type="checkbox" />
-      <label for="toggle-all">Mark all as complete</label>
-      <ul class="todo-list">
-        {listItem}
-        {/** The list append here */}
-      </ul>
-      <footer class="footer">
-        <span class="todo-count">2 Items Left</span>
-        <ul class="filters">
-          <li>
-            <a href="#/" class="selected">
-              All
-            </a>
-          </li>
-          <li>
-            <a href="#/active">Active</a>
-          </li>
-          <li>
-            <a href="#/completed">Completed</a>
-          </li>
+      <header className="header">
+        <h1>todos</h1>
+        <input
+          class="new-todo"
+          placeholder="What needs to be done?"
+          autofocus
+        />
+      </header>
+      <section class="main">
+        <input id="toggle-all" class="toggle-all" type="checkbox" />
+        <label for="toggle-all">Mark all as complete</label>
+        <ul class="todo-list">
+          {listItem}
+          {/** The list append here */}
         </ul>
-        <button class="clear-completed">Clear completed</button>
-      </footer>
-    </section>
-  </div>
-  };
+        <footer class="footer">
+          <span class="todo-count">2 Items Left</span>
+          <ul class="filters">
+            <li>
+              <a href="#/" class="selected">
+                All
+              </a>
+            </li>
+            <li>
+              <a href="#/active">Active</a>
+            </li>
+            <li>
+              <a href="#/completed">Completed</a>
+            </li>
+          </ul>
+          <button class="clear-completed">Clear completed</button>
+        </footer>
+      </section>
+    </div>
+  );
 }
 
 export default App;
